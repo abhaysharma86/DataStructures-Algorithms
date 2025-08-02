f784e4bb39f031ab07eed385a108705c150931bd
Abhay Sharma <sharmaabhay7248@gmail.com>
2025-08-02 21:11:56 +0530

    Rotate Array

---
+++ DataStructures-Algorithms/src/array/RotateArray.java
@@ -0,0 +1,40 @@
+package array;
+
+public class RotateArray {
+    public static void main(String[] args) {
+
+        int arr[] = {1, 2, 3, 4, 5};
+        int d = 5;
+
+        rotateArr(arr, d);
+
+        for(int a : arr){
+            System.out.print(a);
+        }
+
+    }
+    static void rotateArr(int arr[], int d) {
+        int size = arr.length;
+
+        if(size == d){
+//            rotateArray(arr, size);
+        }else if(size > d){
+            rotateArray(arr, d);
+        }else {
+            while(d > size){
+                d = d - size;
+            }
+            rotateArray(arr,d);
+        }
+    }
+
+    private static void rotateArray(int arr[], int y){
+        for(int i = 0; i < y; i++){
+            int a = arr[0];
+            for(int j = 1; j < arr.length; j++){
+                arr[j-1] = arr[j];
+            }
+            arr[arr.length-1] = a;
+        }
+    }
+}
