a8567886917759b1e201ed70275b7aaed918c3d4
Abhay Sharma <sharmaabhay7248@gmail.com>
2025-10-01 21:51:19 +0530

    LinkedList

---
+++ DataStructures-Algorithms/src/linkedList/singlyLinkedList/ReverseLinkedList.java
@@ -0,0 +1,37 @@
+package linkedList.singlyLinkedList;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        Node head = new Node(0);
+        Node first = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        head.next = first;
+        first.next = second;
+        second.next = third;
+        Node reverse = reverse(head);
+        while (reverse != null){
+            System.out.println(reverse.data);
+            reverse = reverse.next;
+        }
+    }
+
+
+    public static Node reverse(Node head){
+
+        Node curr = head;
+        Node prev = null;
+        Node next;
+
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+
+}
